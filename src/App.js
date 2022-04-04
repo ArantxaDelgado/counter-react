4a8bfc32587dfe89a0878ed3c49d7b452d9848d1
ardelgad <adelgadoporras@gmail.com>
2022-04-04 12:49:02 +0200

    jsxDone

--- src/App.js
+++ src/App.js
@@ -1,31 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-
-
-    <div className="App">
-      <div className="App text-white">
-        <h1 className="text-uppercase">counter</h1>
-        <h1 id="counter">0</h1>
-        <div className="col">
-          <Button text="lower count"/>
-          <Button text="add count"/>
-        </div>
-      </div>
+    <div className="App text-white">
+      <CounterBox/>
     </div>
   );
 }
 
 function CounterBox() {
   // TODO: Devuelveme el JSX esperado
-  return 
+  return (<div><h1 className="text-uppercase">counter</h1>
+    <h1 id="counter">0</h1>
+    <div className="col">
+      <Button text="lower count"/>
+      <Button text="add count"/>
+    </div></div>);
 }
 
 function Button(props) {
   // TODO: Devuelveme un botón configurado según los requisitos
   return (<button className="btn counterBtn prevBtn text-uppercase m-2">{props.text}</button>);
+
 }
 
 export default App;
